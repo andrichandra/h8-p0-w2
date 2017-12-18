8bab0d6720aebac2b9dde67c20f5e4e5c6c19e0c
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-18 19:11:33 +0700

    exercise-2

---
+++ exercise-2.js
@@ -0,0 +1,30 @@
+var nama='Mikael';
+var peran='';
+
+var nama='Nina';
+var peran='Ksatria';
+
+var nama='Danu';
+var peran='Tabib';
+
+var nama='Zero';
+var peran='Penyihir';
+
+if (name='Mikael'){
+  console.log("Halo Mikael, pilih peranmu untuk memulai game!");
+}
+
+if (name='Nina'){
+  console.log("Selamat datang di dunia Proxytia, Nina");
+  console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
+}
+
+if (name='Danu'){
+  console.log("Selamat datang di dunia Proxytia, Danu");
+  console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
+}
+
+if (name='Zero'){
+  console.log("Selamat datang di dunia Proxytia, Zero");
+  console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+}
