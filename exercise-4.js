76f42901507d24fc67fff884e1da8d3a097a6a24
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-20 12:48:52 +0700

    exercise-4

---
+++ exercise-4.js
@@ -0,0 +1,15 @@
+console.log('LOOPING PERTAMA');
+var count=2
+
+while (count<=20){
+  console.log(count+' - I love coding');
+  count+=2;
+}
+
+console.log('LOOPING KEDUA');
+var count=20
+
+while (count>=2){
+  console.log(count+' - I will become fullstack developer');
+  count-=2;
+}
