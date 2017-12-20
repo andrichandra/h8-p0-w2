88bb1bb89b2de232e2b10ab20519b0701773107f
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-20 19:31:15 +0700

    exercise-6

--- exercise-6.js
+++ exercise-6.js
@@ -8,21 +8,21 @@ for (var i = 1; i <= 100; i++) {
   }
 }
 
-// pertambahan 2
+// pertambahan 2, kelipatan 3
 for (var i = 1; i <= 100; i+=2) {
   if (i%3===0) {
     console.log(i+" KELIPATAN 3");
   }
 }
 
-// pertambahan 5
+// pertambahan 5, kelipatan 6
 for (var i = 1; i <= 100; i+=5) {
   if (i%6===0) {
     console.log(i+" KELIPATAN 6");
   }
 }
 
-//  pertambahan 9
+//  pertambahan 9, kelipatan 10
 for (var i = 0; i <= 100; i+=9) {
   if (i%10===0) {
     console.log(i+" KELIPATAN 10");
