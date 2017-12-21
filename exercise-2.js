007278a195806839d09c22f9565c43cdccd1f29f
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-21 14:25:45 +0700

    update nama

--- exercise-2.js
+++ exercise-2.js
@@ -1,19 +1,25 @@
-var nama='Zero'
-var peran='Ksatria'
+var nama='Andri'
+var peran='Penyihir'
 
+// nama
+if (nama === '') {
+  console.log('Nama tidak boleh kosong');
+}
 
-if(peran = 'Ksatria') {
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
+else {
+  console.log('Selamat datang di Dunia Proxytia, '+nama);
+}
+
+// peran
+if(peran == 'Ksatria') {
   console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
 }
 
-else if(peran = 'Tabib'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
+else if(peran == 'Tabib'){
   console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
 }
 
-else if(peran = 'Penyihir'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
+else if(peran == 'Penyihir'){
   console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
 }
 
