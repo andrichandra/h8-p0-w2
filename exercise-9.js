22a49a08d4f30fa6d6d52854f832572d92f44849
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-21 17:01:06 +0700

    exercise-9

---
+++ exercise-9.js
@@ -0,0 +1,24 @@
+// Tugas 1
+var a,b,c,d;
+a = ('nama');
+b = ('angka pembilang');
+c = ('angka penyebut');
+d = Number(b)/Number(c)
+console.log('Halo '+a+', '+b+' dibagi '+c+' adalah sama dengan '+d);
+
+// Tugas 2
+var e,f,g;
+e = ('alas');
+f = ('tinggi');
+g = Number(a)*Number(b)/2
+console.log('Luas Segitiga: ' + g);
+
+// Tugas 3
+var i='tahun'
+if (Number(i)%4===0) {
+  console.log(i+' adalah tahun kabisat');
+} else if (Number(i)%400===0) {
+  console.log(i+' adalah tahun kabisat');
+} else {
+  console.log(i+' adalah bukan tahun kabisat');
+}
