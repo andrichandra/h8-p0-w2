d222141a0e112f899081aec9e4edc83f44df39fc
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-21 10:33:22 +0700

    exercise-7

---
+++ exercise-7.js
@@ -0,0 +1,16 @@
+// Tugas 1
+// TULISKAN dan SIMPAN "angka pertama"
+// TULISKAN dan SIMPAN "angka kedua"
+// HITUNG "angka pertama" dikali "angka kedua"
+// SIMPAN hasil hitungan sebelumnya
+// TAMPILKAN hasil hitungan
+
+// Tugas 2
+// TULISKAN dan SIMPAN "Pasti Bisa!"
+// IF ("Saya Belajar JavaScript!")
+// TAMPILKAN "Pasti Bisa!"
+
+// Tugas 3
+// TULISKAN DAN SIMPAN "sebuah angka"
+// TAMPILKAN deret angka habis dibagi 3 dan dimulai dari angka 1
+// ULANGI sampai mencapai angka yang disimpan
