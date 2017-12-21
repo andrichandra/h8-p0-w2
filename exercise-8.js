2a2a42396a18d24d5d9249bf2ae3bebc21a342a8
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-21 15:54:45 +0700

    update

--- exercise-8.js
+++ exercise-8.js
@@ -1,16 +1,14 @@
 // Tugas 1
 function shoutOut() {
-  console.log('Halo Function!');
+  return 'Halo Function!';
 }
 
 // Tugas 2
 function calculateMultiply(num1,num2) {
-  console.log(num1 * num2);
+  return num1 * num2;
 }
 
 // Tugas 3
 function processSentence(name,age,address,hobby) {
-  console.log('Nama saya ' + name + ', umur saya ' + age + ' tahun'+', alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!');
+  return ('Nama saya ' + name + ', umur saya ' + age + ' tahun'+', alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!');
 }
-
-// processSentence('Andri','30','kuningan','coding')
