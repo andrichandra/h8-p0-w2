83a1caf2a5c9d676be61d49db4a5536490d5a475
Andri Chandra <andrichandra@Andris-MacBook-Air.local>
2017-12-21 14:39:07 +0700

    exercise-8

---
+++ exercise-8.js
@@ -0,0 +1,14 @@
+// Tugas 1
+function ShoutOut() {
+  console.log("Halo Function!");
+}
+
+// Tugas 2
+function calculateMultiply(num1,num2) {
+  return num1 * num2);
+}
+
+// Tugas 3
+function processSentence(name,age,adress,hobby) {
+  console.log("Nama saya "+name+", umur saya "+age+" tahun"+", alamat saya di "+address+", dan saya punya hobby yaitu "+hobby+"!");
+}
